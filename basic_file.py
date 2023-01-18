a25a1b19a0a653a55b4d3f0ae85fbf85c74e7719
Alexandr Kuzmich <92593750+xofmdo@users.noreply.github.com>
2023-01-19 02:15:21 +0300

    await blocks execution

--- basic_file.py
+++ basic_file.py
@@ -14,6 +14,8 @@ async def hello_world_message() -> str:
 async def main() -> None:
     message = await hello_world_message()
     one_plus_one = await add_one(1)
+    # await блокирует выполнение, т.е. мы приостанавливаем всю сопрограмму на
+    # время, пока выражение await не вернет управление.
     print(one_plus_one)
     print(message)
 
